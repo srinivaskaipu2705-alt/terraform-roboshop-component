f5e15b909b3fa0e8d844b362e350f609de5fe98a
srini <srinivaskaipu2705@gmail.com>
2026-04-22 13:02:42 -0400

    terraform

--- data.tf
+++ data.tf
@@ -27,10 +27,6 @@ data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_id" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_id"
-}
-
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
